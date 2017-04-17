775874abafac2df2870b4b56d094b5081dcf86dc
Magnum26 <matthewahillman@gmail.com>
2017-04-17 19:36:18 +0100

    formatting and adding in JSON stringify

--- weightconversion.js
+++ weightconversion.js
@@ -4,41 +4,34 @@ var weightConv = {
 	childNameElement: document.getElementById('childName'),
 	weightInput: document.getElementById('weightInput'),
 	currentWeightInputBox: document.getElementById('weightInKg'),
+	dateInput: document.getElementById('dateSelect'),
+	lbs: 453.592, // 1lbs to grams
+	oz: 28.3495, // 1 oz to grams
 	init: function event()
 	{
 		weightConv.button.addEventListener('click', weightConv.calculateValues);
 	},
-
-	lbs: 453.592, // 1lbs to grams
-	oz: 28.3495, // 1 oz to grams
-	currentWeight: 0, // will be a user input with add button
-	kg2grams: 0, // current Weight to grams e.g 3.5kg to 3500g
-
 	calculateValues: function calculateValues()
 	{
-		weightConv.currentWeight = weightConv.currentWeightInputBox.value; // will be a user input with add button
+		weightConv.currentWeight = weightConv.currentWeightInputBox.value;
 		weightConv.kg2grams = weightConv.currentWeight * 1000; // current Weight to grams e.g 3.5kg to 3500g
-
 		// Print out the process to the console
 		var printOut = {
-			weightinkg: "Weight in kg: " + weightConv.currentWeight + "kg",
-			weightingrams: "Weight in Grams: " + weightConv.kg2grams + "g",
-			weightinlbs: "Weight in lbs: " + weightConv.lbsTrunc(),
-			lbstruncate: "Lbs Truncate: " + weightConv.lbsFloor(),
-			ounces: "Ounces: " + weightConv.ozFloored(),
-			oztruncate: "Oz Truncate: " + weightConv.ozTrunc(),
-			child: weightConv.getChildName() + " is: ",
+			date: weightConv.getTodaysDate(),
+			child: weightConv.getChildName(),
 			result: weightConv.lbsFloor() + "lbs " + weightConv.ozTrunc() + "ozs"
 		};
-
 		if (weightConv.currentWeight > 0)
 		{
 			console.log(printOut);
-			document.getElementById("pResults").innerHTML = printOut.child + printOut.result;
+			document.getElementById("pResults").innerHTML = "Todays date is " + printOut.date + " " + printOut.child + " is: " + printOut.result;
 			weightConv.currentWeightInputBox.value = ""; //Clear the box afterwards
 		}
+		// convert to JSON for moving to DB
+		var store = window.JSON.stringify(printOut);
+		console.log(store);
+		//
 	},
-
 	getChildName: function getChildName()
 	{
 		return document.getElementById('childName').value;
@@ -67,8 +60,11 @@ var weightConv = {
 	ozTrunc: function ozTrunc()
 	{
 		return Math.trunc(weightConv.ozFloored());
+	},
+	getTodaysDate: function getTodaysDate()
+	{
+		return new Date().toLocaleDateString('en-GB');
 	}
-
 };
 
 weightConv.init();
