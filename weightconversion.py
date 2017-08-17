3e3d29178de58ca3af424557d2d32b53c53dee8e
Magnum26 <matthewahillman@gmail.com>
2017-08-17 16:37:28 +0100

    v1.0.1

--- weightconversion.py
+++ weightconversion.py
@@ -1,11 +1,20 @@
-import math
+import math, json, time
 from datetime import datetime
+
 lbs = 453.592
 oz = 28.3495
 date = datetime.now()
 date_format =  '%s/%s/%s' % (date.month, date.day, date.year)
-name = input("Name: ")
-current_weight = float(input("Enter the weight in kg"))
+
+while True:
+    name = input("Name: ")
+    if name == 'Oliver' or name == 'Ethan':
+        print("Hello " + name)
+        break
+    else:
+        print("Sorry that name can\'t be processed, please try again...")
+
+current_weight = float(input("Enter the weight in kg: "))
 kg_to_grams = current_weight * 1000
 
 def lbs_conv():
@@ -17,4 +26,18 @@ def ounces_conv():
 def output():
     return "\n"+ name + " is: " + str(lbs_conv()) + " lbs " + str(ounces_conv()) + " ozs"
 
-print(output(), "\nTodays date is: " + date_format)
+print("\nTodays date is: " + date_format  + output())
+
+time.sleep(5)
+
+results = (
+{
+    'date_today': date_format,
+    'name': name,
+    'lbs': lbs_conv(),
+    'ozs': ounces_conv()
+})
+with open('weights.json', 'a') as save:
+    json.dump(results, save, indent = 4)
+    save.close()
+print("Saved to file")
